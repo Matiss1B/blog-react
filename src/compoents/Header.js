55a13f8d5c045f9eb5e99fe2bc619da242a810d7
Matiss1B <103557980+Matiss1B@users.noreply.github.com>
2023-04-01 09:12:41 +0300

    header

--- src/compoents/Header.js
+++ src/compoents/Header.js
@@ -3,9 +3,20 @@ import "../assets/scss/App.css";
 
 function Header() {
     return (
-        <header className="header">
-            <NavLink to="/" >Home</NavLink>
-            <NavLink to="/add">Add blog</NavLink>
+        <header className="header w-100 flex gap1 col shadow-light pad2">
+            <div className="title w-100 middle">
+                <h1>BlogIt</h1>
+            </div>
+            <div className="w-100 flex middle">
+                <div className="flex gap5">
+                    <NavLink className="nav-link" to="/" ><p>Home</p></NavLink>
+                    <NavLink className="nav-link" to="/food"><p>Food</p></NavLink>
+                    <NavLink className="nav-link" to="/fashion"><p>Fashion</p></NavLink>
+                    <NavLink className="nav-link" to="/cars"><p>Cars</p></NavLink>
+                    <NavLink className="nav-link" to="/add"><p>Add blog</p></NavLink>
+
+                </div>
+            </div>
         </header>
     );
 }
